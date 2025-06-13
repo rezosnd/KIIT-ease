b0e5c0fa5e612072eaca4aaf4ab4b5d623873860
REHAN SUMAN <rehansuman41008@gmail.com>
2025-06-13 14:01:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { Navbar } from "@/components/navbar"
+import Navbar from "@/components/navbar";
 import { Footer } from "@/components/footer"
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from "@/components/auth-provider"
